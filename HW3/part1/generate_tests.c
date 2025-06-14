d6692ec3787da7a602c988744953a2980f7021a7
UriKH <urikh@icloud.com>
2025-06-14 13:18:13 +0300

    fixing some bugs

---
+++ HW3/part1/generate_tests.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+    if (argc > 1){
+        FILE* fd = fopen(argv[1], "w");
+        if (fd == NULL){
+            printf("could not open %s\n", argv[1]);
+            exit(1);
+        }
+        const char* msg = "I love ATAM\n";
+        fputs(msg, fd);
+        fclose(fd);
+    }
+    else{
+        printf("not enough arguments\n");
+    }
+    return 0;
+}
